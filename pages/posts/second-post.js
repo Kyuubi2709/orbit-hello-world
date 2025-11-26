590e109974a3b0c6df81673e0c438b836e45c185
Kyuubi2709 <cryptokyuubi@protonmail.com>
2025-11-26 15:51:19 +0100

    Create second-post.js

---
+++ pages/posts/second-post.js
@@ -0,0 +1,14 @@
+export default function SecondPost() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Second Test Post 🚀</h1>
+      <p>This is a brand-new post created to test CI/CD auto-deploys on Orbit.</p>
+
+      <p>When you see this page live, your pipeline is working!</p>
+
+      <a href="/" style={{ color: "#0070f3" }}>
+        ← Back to home
+      </a>
+    </main>
+  );
+}
