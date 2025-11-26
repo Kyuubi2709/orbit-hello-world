20a9f5660a9e787de1697d6c62b1430ee750eeae
Kyuubi2709 <cryptokyuubi@protonmail.com>
2025-11-26 14:17:19 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Hello World 👋</h1>
+      <p>Welcome to my simple Next.js blog running on Flux Orbit.</p>
+
+      <a href="/posts/first-post" style={{ color: "#0070f3" }}>
+        Read my first post →
+      </a>
+    </main>
+  );
+}
